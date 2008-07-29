d272a71de1bd7fc4974c53b358b1d74e50709429
Richard Körber <github@shredzone.org>
2008-07-29 23:39:54 +0200

    Browser now handles large resultsets.

--- src/org/shredzone/repowatch/web/util/BrowserData.java
+++ src/org/shredzone/repowatch/web/util/BrowserData.java
@@ -16,22 +16,23 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * $Id: BrowserData.java 181 2008-07-22 11:35:11Z shred $
+ * $Id: BrowserData.java 200 2008-07-29 21:39:54Z shred $
  */
 
 package org.shredzone.repowatch.web.util;
+
+import java.util.Iterator;
 
 /**
  * Contains all data required to show a page browser.
  * 
  * @author Richard "Shred" Körber
- * @version $Revision: 181 $
+ * @version $Revision: 200 $
  */
 public class BrowserData {
     private int pagecount;
     private int page;
     private long resultcount;
-    private boolean dotted;
     private String baseurl;
     private String pageparam = "page";
 
@@ -89,12 +90,6 @@ public class BrowserData {
     public void setResultcount(long resultcount) { this.resultcount = resultcount; }
 
     /**
-     * Internal flag for the browser. Do not use.
-     */
-    public boolean isDotted()                   { return dotted; }
-    public void setDotted(boolean dotted)       { this.dotted = dotted; }
-
-    /**
      * Returns the URL to be loaded when another page is to be shown. This is
      * usually the URL of the page currently shown.
      * 
@@ -111,5 +106,161 @@ public class BrowserData {
      */
     public String getPageparam()                { return pageparam; }
     public void setPageparam(String pageparam)  { this.pageparam = pageparam; }
+    
+    /**
+     * Returns an iterator that iterates thru a sequence of page numbers.
+     * The sequence can be used in the browser fragment to select other
+     * page numbers.
+     * <p>
+     * The current state of the BrowserData is frozen in this iterator.
+     * <p>
+     * Note that the Iterator starts counting from 1 to make the result
+     * readable for humans, while the browser itself counts the page starting
+     * from zero.
+     * 
+     * @return  Iterator that iterates through a sequence of page numbers
+     */
+    public Iterator<Integer> getPageIterator() {
+        return new PageNumberIterator(
+                getPage(),
+                getPagesteps(),
+                getPagecount()
+        );
+    }
 
+    /**
+     * This class provides the page number iterator.
+     */
+    private static class PageNumberIterator implements Iterator<Integer> {
+        /*TODO: Make this configurable some day... */
+        private final int BORDER_PAGES = 3;
+        private final int CENTER_PAGES = 4;
+
+        private final int pageDisplayed;
+        private final int steps;
+        private final int pageCount;
+        private int currentPage = 1;
+
+        /**
+         * Create a new page number iterator.
+         * 
+         * @param page      Page that is currently shown, starting from 0.
+         * @param steps     Large step rate between both ends of the scale.
+         *      Usually pass the result of {@link BrowserData#getPagesteps()}
+         *      here.
+         * @param count     Total number of pages.
+         */
+        public PageNumberIterator(int page, int steps, int count) {
+            this.pageDisplayed = page;
+            this.steps = steps;
+            this.pageCount = count;
+        }
+
+        /**
+         * Returns true if there is another page number to be shown.
+         * 
+         * @return <code>true</code>: has more page numbers
+         */
+        @Override
+        public boolean hasNext() {
+            return currentPage <= pageCount;
+        }
+
+        /**
+         * Returns the next page number in the sequence.
+         * 
+         * @return Next page number
+         */
+        @Override
+        public Integer next() {
+            int result = currentPage;
+            
+            int nextPage = pageCount + 1;
+            
+            // The next page number is the nearest proposal that is still
+            // within the scale, and is at least one page ahead.
+            int nextStep = proposeNextStep(currentPage);
+            if (nextStep > currentPage && nextStep < nextPage) nextPage = nextStep;
+            
+            int nextStart = proposeNextStart(currentPage);
+            if (nextStart > currentPage && nextStart < nextPage) nextPage = nextStart;
+            
+            int nextEnd = proposeNextEnd(currentPage);
+            if (nextEnd > currentPage && nextEnd < nextPage) nextPage = nextEnd;
+
+            int nextCenter = proposeNextCenter(currentPage);
+            if (nextCenter > currentPage && nextCenter < nextPage) nextPage = nextCenter;
+
+            currentPage = nextPage;
+            return result;
+        }
+
+        /**
+         * Unsupported operation!
+         */
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        
+        /**
+         * Propose the next general page. This is the next page of the given
+         * step rate. (E.g. if the current page is 184 and the step rate is 100,
+         * the next proposed page is 200). 
+         * 
+         * @param current   Current page
+         * @return  Next proposed page
+         */
+        private int proposeNextStep(int current) {
+            return ((current / steps) + 1) * steps;
+        }
+
+        /**
+         * Proposes the next page on the left side of the scale. This are
+         * the first {@link #BORDER_PAGES} number of pages.
+         * 
+         * @param current   Current page
+         * @return  Next proposed page
+         */
+        private int proposeNextStart(int current) {
+            if (current < BORDER_PAGES) {
+                return current + 1;
+            } else {
+                return pageCount + 1;
+            }
+        }
+        
+        /**
+         * Proposes the next page on the right side of the scale. This are
+         * the last {@link #BORDER_PAGES} number of pages.
+         * 
+         * @param current   Current page
+         * @return  Next proposed page
+         */
+        private int proposeNextEnd(int current) {
+            if (current > pageCount - BORDER_PAGES) {
+                return current + 1;
+            } else {
+                return pageCount - BORDER_PAGES + 1;
+            }
+        }
+        
+        /**
+         * Proposes a number of pages around the current page number, with
+         * the current page number being in the center. Usually there are
+         * {@link #CENTER_PAGES} pages each to the left and right of the
+         * current page number.
+         * 
+         * @param current   Current page
+         * @return  Next proposed page
+         */
+        private int proposeNextCenter(int current) {
+            if (Math.abs(current - pageDisplayed) <= CENTER_PAGES) {
+                return current + 1;
+            } else {
+                return pageDisplayed - CENTER_PAGES + 1;
+            }
+        }
+    }
+    
 }
